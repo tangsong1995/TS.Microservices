841005ae0ab78040a1bfe24af2d4e7db3ae76877
tangsong <tangs@zhulong.com.cn>
2021-10-26 19:10:22 +0800

    prometheus

--- Deploy/infrastructure/init/prometheus-operator.ps1
+++ Deploy/infrastructure/init/prometheus-operator.ps1
@@ -1,2 +1,2 @@
 ﻿helm repo update
-helm upgrade prometheus-operator stable/prometheus-operator --version 8.10.0 --values .\prometheus-operator\values.yaml  --namespace kube-system
+helm install prometheus-operator stable/prometheus-operator --version 8.10.0 --values .\prometheus-operator\values.yaml  --namespace kube-system
