b5b06fe5517e9fdef6612b79d648b64e11f360fd
tangsong1995 <31871049+tangsong1995@users.noreply.github.com>
2023-05-04 15:45:13 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ Param(
 docker build --no-cache  -f  .\App\TS.Microservices.WebApi\Dockerfile -t ts-microservices-webapi:$tag .
 docker build --no-cache  -f  .\ApiGateways\TS.Microservices.Mobile.Gateway\Dockerfile -t ts-microservices-mobile-gateway:$tag .
 docker build --no-cache  -f  .\ApiGateways\TS.Microservices.Mobile.ApiAggregator\Dockerfile -t ts-microservices-mobile-apiaggregator:$tag .
-docker build --no-cache  -f  .\Monitor\TS.Microservices.HealthChecksHost\Dockerfile -t geektime-healthcheckshost:$tag .
+docker build --no-cache  -f  .\Monitor\TS.Microservices.HealthChecksHost\Dockerfile -t ts-microservices-healthcheckshost:$tag .
 
 "Any key to exit"  ;
 Read-Host | Out-Null ;
